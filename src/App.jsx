9a2160649ac0cbe9f4440d0dadab560dca751920
Shobhit Verma <vshobhit125@gmail.com>
2024-05-12 21:21:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Button} from 'react-bootstrap'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2>E- Commerce Dashboard</h2>
+        <button>simple</button>
+        <Button>Bootstrap</Button>
+    </>
+  )
+}
+
+export default App
